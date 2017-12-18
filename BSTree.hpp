fc90804672bac6903347a2a028e7ee519a7f1a70
Pavlos Sakoglou <23369146+PavlosSakoglou@users.noreply.github.com>
2017-12-18 12:54:22 -0500

    Uploading the header file
    Header file for BSTree with all methods and definitions

---
+++ BSTree.hpp
@@ -0,0 +1,64 @@
+/*
+*	© Superharmonic Technologies
+*	Pavlos Sakoglou
+*
+*  ================================================
+*
+*	Standard Binary Search Tree API Definition
+*
+*/
+
+#ifndef BSTREE_HPP
+#define BSTREE_HPP
+
+#include <iostream>
+
+// Binary Tree Node
+template <class Type>
+struct BTreeNode {
+	BTreeNode * left;
+	BTreeNode * right;
+	Type data;
+	BTreeNode(Type d = 0) : data(d), left(nullptr), right(nullptr) {}
+};
+
+// Binary Search Tree class
+template <class Type>
+class BSTree {
+public:
+	BSTree();
+	~BSTree();
+
+	// Insert method
+	void insert(Type key);
+
+	// Delete tree
+	void clear();
+
+	// Search method
+	bool find(Type key);
+
+	// Get method
+	BTreeNode<Type> * get(Type key);
+
+	// Traverse in order
+	void traverse();
+
+private:
+
+	// Private recursive methods
+	BTreeNode<Type> * get(Type key, BTreeNode<Type> * r);
+	void insert(Type key, BTreeNode<Type> * r);
+	bool find(Type key, BTreeNode<Type> * r);
+	void traverse(BTreeNode<Type> * r);
+	void clear(BTreeNode<Type> * r);
+
+	BTreeNode<Type> * root;
+};
+
+#ifndef BSTREE_CPP
+#include "BSTree.cpp"
+#endif
+
+
+#endif //!BSTREE_HPP
